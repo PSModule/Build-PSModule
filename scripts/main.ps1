8fb0cd2e901f5f48b6cde749729228d129f5739f
Marius Storhaug <marstor@hotmail.com>
2023-08-31 16:48:29 +0200

    fix

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -11,14 +11,15 @@ Write-Verbose "$Task`: Message: $Message"
 Write-Verbose "$Task`: Get required modules"
 Write-Verbose "$Task`: Combine files to build module"
 Write-Verbose "$Task`: Generate module manifest"
+$manifestPath = '.\outputs\test.psd1'
 $params = @{
-    Path          = '.\output\test.psd1'
+    Path          = $manifestPath
     Guid          = $(New-Guid).Guid
     Author        = 'Marius Storhaug'
     ModuleVersion = '0.0.1'
     Description   = 'Test module'
 }
-New-Item -Path '.\outputs\test.psd1' -Force -ItemType File
+New-Item -Path $manifestPath -Force -ItemType File
 New-ModuleManifest @params -Verbose
 
 
