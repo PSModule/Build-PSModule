9e0edb29c14f535bad748e97686ffc1461ccc927
Marius Storhaug <marstor@hotmail.com>
2024-02-18 17:43:33 +0100

    Add all function params to action

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,8 +2,11 @@
 Write-Output '-------------------------------------------'
 Write-Output 'Action inputs:'
 $params = @{
-    Verbose = $env:Verbose -eq 'true'
-    WhatIf  = $env:WhatIf -eq 'true'
+    Name       = $env:Name
+    Path       = $env:Path
+    OutputPath = $env:OutputPath
+    Verbose    = $env:Verbose -eq 'true'
+    WhatIf     = $env:WhatIf -eq 'true'
 }
 $params.GetEnumerator() | Sort-Object -Property Name
 Write-Output '::endgroup::'
