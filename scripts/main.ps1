2350c728aa76ed96249c75d2afd5083569909979
Marius Storhaug <marstor@hotmail.com>
2024-02-18 17:12:10 +0100

    Fix build action

---
+++ scripts/main.ps1
@@ -0,0 +1,11 @@
+﻿Write-Output '::group::Initializing...'
+Write-Output '-------------------------------------------'
+Write-Output 'Action inputs:'
+$params = @{
+    Verbose = $env:Verbose -eq 'true'
+    WhatIf  = $env:WhatIf -eq 'true'
+}
+$params.GetEnumerator() | Sort-Object -Property Name
+Write-Output '::endgroup::'
+
+Build-PSModule @params
