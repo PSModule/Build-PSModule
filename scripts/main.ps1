108865b79f6681255da0e497fd76ffddacd310d7
Marius Storhaug <marstor@hotmail.com>
2024-02-24 13:32:54 +0100

    Align with Action Design

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,24 +1,30 @@
-﻿$VerbosePreference = 'Continue'
+﻿#REQUIRES -Modules Utilities
 
-Write-Output '##[group]Loading helper scripts'
-Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse | ForEach-Object {
-    Write-Host "[$($_.FullName)]"
-    . $_.FullName
-}
-Write-Output '##[endgroup]'
+[CmdletBinding()]
+param()
 
-$name = [string]::IsNullOrEmpty($env:Name) ? $env:GITHUB_REPOSITORY -replace '.+/' : $env:Name
+Start-LogGroup 'Loading helper scripts'
+Get-ChildItem -Path (Join-Path -Path $env:GITHUB_ACTION_PATH -ChildPath 'scripts' 'helpers') -Filter '*.ps1' -Recurse |
+    ForEach-Object { Write-Verbose "[$($_.FullName)]"; . $_.FullName }
+Stop-LogGroup
 
-$sourceModulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:Path $name
+Start-LogGroup 'Loading inputs'
+$env:GITHUB_REPOSITORY_NAME = $env:GITHUB_REPOSITORY -replace '.+/'
+Set-GitHubEnv -Name 'GITHUB_REPOSITORY_NAME' -Value $env:GITHUB_REPOSITORY_NAME
+$moduleName = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ?$env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
+$sourceModulePath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path $moduleName
+Write-Verbose "Module name:         [$moduleName]"
+Write-Verbose "Source module path:  [$sourceModulePath]"
 if (-not (Test-Path -Path $sourceModulePath)) {
     throw "Module path [$sourceModulePath] does not exist."
 }
 
-$modulesOutputPath = Join-Path $env:GITHUB_WORKSPACE $env:ModulesOutputPath
-$docsOutputPath = Join-Path $env:GITHUB_WORKSPACE $env:DocsOutputPath
-
+$modulesOutputPath = Join-Path $env:GITHUB_WORKSPACE $env:GITHUB_ACTION_INPUT_ModulesOutputPath
+Write-Verbose "Modules output path: [$modulesOutputPath]"
+$docsOutputPath = Join-Path $env:GITHUB_WORKSPACE $env:GITHUB_ACTION_INPUT_DocsOutputPath
+Write-Verbose "Docs output path:    [$docsOutputPath]"
+Stop-LogGroup
 $params = @{
-    Name              = $name
     SourcePath        = $sourceModulePath
     ModulesOutputPath = $modulesOutputPath
     DocsOutputPath    = $docsOutputPath
