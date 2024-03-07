c76369dceb335059e61609883e495f2263f27e6a
Marius Storhaug <marstor@hotmail.com>
2024-03-07 23:57:47 +0100

    test

--- tests/src/PSModule/modules/OtherPSModule.psm1
+++ tests/src/PSModule/modules/OtherPSModule.psm1
@@ -4,19 +4,5 @@
         [Parameter(Mandatory)]
         [string] $Name
     )
-
-    $module = @{
-        Name = $Name
-        Version = '1.0.0'
-        Functions = @(
-            @{
-                Name = 'Get-OtherPSModule'
-                Synopsis = 'Gets the OtherPSModule.'
-                Description = 'This function will get the OtherPSModule.'
-                Example = 'Get-OtherPSModule -Name "OtherPSModule"'
-            }
-        )
-    }
-
-    $module
+    Write-Output "Hello, $Name!"
 }
