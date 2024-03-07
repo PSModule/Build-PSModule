bcc1c8fec535806e563e8995573059848ff97a98
Marius Storhaug <marstor@hotmail.com>
2024-03-07 23:11:36 +0100

    fix

--- scripts/helpers/Build/Get-PSModuleRootModule.ps1
+++ scripts/helpers/Build/Get-PSModuleRootModule.ps1
@@ -19,29 +19,28 @@
     $candidateFiles = Get-ChildItem -Path $SourceFolderPath -File | Where-Object { $_.BaseName -like $_.Directory.BaseName }
     $rootModuleExtensions = '.psm1', '.ps1', '.dll', '.cdxml', '.xaml'
 
-    $rootModuleExtensions | ForEach-Object {
+    $rootModule = $rootModuleExtensions | ForEach-Object {
         $extension = $_
         $candidateFiles | ForEach-Object { Where-Object { $_.Extension -eq $extension } }
     } | Select-Object -First 1 -ExpandProperty Name
 
+    if (-not $rootModule) {
+        Write-Verbose 'No RootModule found'
+    }
 
-if (-not $rootModule) {
-    Write-Verbose 'No RootModule found'
-}
-$rootModule
-$moduleType = switch -Regex ($RootModule) {
-    '\.(ps1|psm1)$' { 'Script' }
-    '\.dll$' { 'Binary' }
-    '\.cdxml$' { 'CIM' }
-    '\.xaml$' { 'Workflow' }
-    default { 'Manifest' }
-}
-Write-Verbose "[$manifestPropertyName] - [$moduleType]"
+    $moduleType = switch -Regex ($RootModule) {
+        '\.(ps1|psm1)$' { 'Script' }
+        '\.dll$' { 'Binary' }
+        '\.cdxml$' { 'CIM' }
+        '\.xaml$' { 'Workflow' }
+        default { 'Manifest' }
+    }
+    Write-Verbose "[$manifestPropertyName] - [$moduleType]"
 
-$supportedModuleTypes = @('Script', 'Manifest')
-if ($moduleType -notin $supportedModuleTypes) {
-    Write-Warning "[$moduleType] - Module type not supported"
-}
+    $supportedModuleTypes = @('Script', 'Manifest')
+    if ($moduleType -notin $supportedModuleTypes) {
+        Write-Warning "[$moduleType] - Module type not supported"
+    }
 
-$rootModule
+    $rootModule
 }
