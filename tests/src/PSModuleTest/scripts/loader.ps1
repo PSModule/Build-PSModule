50c3f3ff6219deefcc733f0cda241f09dc1626f5
Marius Storhaug <marstor@hotmail.com>
2024-03-31 11:16:01 +0200

    fix

--- tests/src/PSModuleTest/scripts/loader.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-Verbose '-------------------------' -Verbose
-Write-Verbose '---  THIS IS A LOADER ---' -Verbose
-Write-Verbose '-------------------------' -Verbose
-
-$MyInvocation | ConvertTo-Json
