db5134ef00e1a4984b164c43110a06a6f80b9e57
Marius Storhaug <marstor@hotmail.com>
2024-08-16 22:21:14 +0200

    Add functionality to export variables

--- scripts/helpers/Build/Get-PSModuleVariablesToExport.ps1
+++ scripts/helpers/Build/Get-PSModuleVariablesToExport.ps1
@@ -20,16 +20,24 @@
 
     $moduleName = Split-Path -Path $SourceFolderPath -Leaf
     $manifestFileName = "$moduleName.psd1"
-    $manifestFilePath = Join-Path -Path $SourceFolderPath $manifestFileName
-
+    $manifestFilePath = Join-Path -Path $SourceFolderPath -ChildPath $manifestFileName
     $manifest = Get-ModuleManifest -Path $manifestFilePath -Verbose:$false
-
     Write-Verbose "[$manifestPropertyName]"
 
+    $variableFolderPath = Join-Path -Path $SourceFolderPath -ChildPath 'public'
+    $scriptFilePaths = Get-ChildItem -Path $variableFolderPath -Recurse -File -Filter *.ps1 | Select-Object -ExpandProperty FullName
+
+    $collectedVariablesToExport = $scriptFilePaths | ForEach-Object {
+        Get-RootLevelVariables -Ast [System.Management.Automation.Language.Parser]::ParseFile($_, [ref]$null, [ref]$null)
+    }
+
     $variablesToExport = (($manifest.VariablesToExport).count -eq 0) -or ($manifest.VariablesToExport | IsNullOrEmpty) ? '' : $manifest.VariablesToExport
+    $variablesToExport += $collectedVariablesToExport | Sort-Object -Unique
+    
     $variablesToExport | ForEach-Object {
         Write-Verbose "[$manifestPropertyName] - [$_]"
     }
 
     $variablesToExport
 }
+
