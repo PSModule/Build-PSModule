7dc77cac6dc535cb849f936d17d67b1a87d01355
Marius Storhaug <marstor@hotmail.com>
2024-12-06 23:02:47 +0100

    🚀 [Feature]: Structure docs similar to the source code (#75)
    ## Description
    
    - Structure docs similar to the source code
      - Fixes #64
    - Run tests with `ubuntu-24.04`.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/helpers/Build/Build-PSModuleDocumentation.ps1
+++ scripts/helpers/Build/Build-PSModuleDocumentation.ps1
@@ -22,6 +22,10 @@ function Build-PSModuleDocumentation {
         [Parameter(Mandatory)]
         [string] $ModuleName,
 
+        # Path to the folder where the module source code is located.
+        [Parameter(Mandatory)]
+        [System.IO.DirectoryInfo] $ModuleSourceFolder,
+
         # Folder where the documentation for the modules should be outputted. 'outputs/docs/MyModule'
         [Parameter(Mandatory)]
         [System.IO.DirectoryInfo] $DocsOutputFolder
@@ -32,6 +36,9 @@ function Build-PSModuleDocumentation {
         Import-Module -Name $ModuleName -Force -RequiredVersion '999.0.0'
         Write-Verbose ($ModuleName | Get-Module)
         $null = New-MarkdownHelp -Module $ModuleName -OutputFolder $DocsOutputFolder -Force -Verbose
+    }
+
+    LogGroup 'Build docs - Fix markdown code blocks' {
         Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
             $content = Get-Content -Path $_.FullName
             $fixedOpening = $false
@@ -49,6 +56,9 @@ function Build-PSModuleDocumentation {
             }
             $newContent | Set-Content -Path $_.FullName
         }
+    }
+
+    LogGroup 'Build docs - Fix markdown escape characters' {
         Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
             $content = Get-Content -Path $_.FullName -Raw
             $content = $content -replace '\\`', '`'
@@ -58,6 +68,36 @@ function Build-PSModuleDocumentation {
             $content = $content -replace '\\\>', '>'
             $content = $content -replace '\\\\', '\'
             $content | Set-Content -Path $_.FullName
+        }
+    }
+
+    # Markdown files are called the same as the source files, but with a .md extension.
+    # They are all located flat in the docsoutputfolder.
+    # I want the markdown files to be moved in the same folder structure as the source files.
+    LogGroup 'Build docs - Structure markdown files to match source files' {
+        $PublicFunctions = Join-Path $ModuleSourceFolder.FullName 'functions\public' | Get-Item
+        Get-ChildItem -Path $DocsOutputFolder -Recurse -Force -Include '*.md' | ForEach-Object {
+            $file = $_
+            Write-Verbose "Processing:        $file"
+
+            # find the source code file that matches the markdown file
+            $scriptPath = Get-ChildItem -Path $PublicFunctions -Recurse -Force | Where-Object { $_.Name -eq ($file.BaseName + '.ps1') }
+            Write-Verbose "Found script path: $scriptPath"
+            $docsFilePath = ($scriptPath.FullName).Replace($PublicFunctions.FullName, $DocsOutputFolder.FullName).Replace('.ps1', '.md')
+            Write-Verbose "Doc file path:     $docsFilePath"
+            $docsFolderPath = Split-Path -Path $docsFilePath -Parent
+            New-Item -Path $docsFolderPath -ItemType Directory -Force
+            Move-Item -Path $file.FullName -Destination $docsFilePath -Force
+        }
+        # Get the MD files that are in the public functions folder and move them to the same place in the docs folder
+        Get-ChildItem -Path $PublicFunctions -Recurse -Force -Include '*.md' | ForEach-Object {
+            $file = $_
+            Write-Verbose "Processing:        $file"
+            $docsFilePath = ($file.FullName).Replace($PublicFunctions.FullName, $DocsOutputFolder.FullName)
+            Write-Verbose "Doc file path:     $docsFilePath"
+            $docsFolderPath = Split-Path -Path $docsFilePath -Parent
+            New-Item -Path $docsFolderPath -ItemType Directory -Force
+            Move-Item -Path $file.FullName -Destination $docsFilePath -Force
         }
     }
 
