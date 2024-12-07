05b644cfd1aea930db326ebcaab589831bcbdb7d
Marius Storhaug <marstor@hotmail.com>
2024-12-07 15:34:05 +0100

    🩹 [Patch]: Adds a module variable called `$script:PSModuleInfo` (#78)
    ## Description
    
    - Adds a module variable called `$script:PSModuleInfo` with contents
    from the module manifest. Loaded via `Test-ModuleManifest`.
      - Fixes #71 
    - Prints the contents of `$script:PSModuleInfo` with `-Debug` flag.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/helpers/Build/Build-PSModuleRootModule.ps1
+++ scripts/helpers/Build/Build-PSModuleRootModule.ps1
@@ -137,6 +137,13 @@ $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
 param()
 '@
         }
+
+        # Add a variable $script:PSModuleInfo to the root module, which contains the module manifest information.
+        Add-Content -Path $rootModuleFile -Force -Value @'
+$baseName = [System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)
+$script:PSModuleInfo = Test-ModuleManifest -Path "$PSScriptRoot\$baseName.psd1"
+$script:PSModuleInfo | Format-List | Out-String -Stream | ForEach-Object { Write-Debug $_ }
+'@
         #endregion - Module header
 
         #region - Module post-header
