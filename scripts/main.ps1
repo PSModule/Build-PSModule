4a43798832f24d98d859521b14b8d31154aedd70
Marius Storhaug <marstor@hotmail.com>
2025-02-07 04:02:57 +0100

    🚀 [Feature]: Adds customizable build steps (#103)
    ## Description
    
    This pull request adds a step to the `scripts/main.ps1` file to run
    local build scripts, as well as a small addition to the `README.md`
    file.
    
    Run custom build scripts
    
    * Added a new log group to run local build scripts found in the
    repository, sorted alphabetically by file name `*build.ps1`.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,11 +3,11 @@
 [CmdletBinding()]
 param()
 
-$path = (Join-Path -Path $PSScriptRoot -ChildPath 'helpers')
+$path = (Join-Path -Path $PSScriptRoot -ChildPath 'helpers') | Get-Item | Resolve-Path -Relative
 LogGroup "Loading helper scripts from [$path]" {
-    Get-ChildItem -Path $path -Filter '*.ps1' -Recurse | ForEach-Object {
-        Write-Host "[$($_.FullName)]"
-        . $_.FullName
+    Get-ChildItem -Path $path -Filter '*.ps1' -Recurse | Resolve-Path -Relative | ForEach-Object {
+        Write-Host "$_"
+        . $_
     }
 }
 
@@ -15,7 +15,7 @@ LogGroup 'Loading inputs' {
     $moduleName = ($env:GITHUB_ACTION_INPUT_Name | IsNullOrEmpty) ? $env:GITHUB_REPOSITORY_NAME : $env:GITHUB_ACTION_INPUT_Name
     Write-Host "Module name:         [$moduleName]"
 
-    $moduleSourceFolderPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path $moduleName
+    $moduleSourceFolderPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path/$moduleName
     if (-not (Test-Path -Path $moduleSourceFolderPath)) {
         $moduleSourceFolderPath = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath $env:GITHUB_ACTION_INPUT_Path
     }
@@ -28,6 +28,19 @@ LogGroup 'Loading inputs' {
     Write-Host "Modules output path: [$modulesOutputFolderPath]"
     $docsOutputFolderPath = Join-Path $env:GITHUB_WORKSPACE $env:GITHUB_ACTION_INPUT_DocsOutputPath
     Write-Host "Docs output path:    [$docsOutputFolderPath]"
+}
+
+LogGroup 'Build local scripts' {
+    Write-Host 'Execution order:'
+    $scripts = Get-ChildItem -Filter '*build.ps1' -Recurse | Sort-Object -Property Name | Resolve-Path -Relative
+    $scripts | ForEach-Object {
+        Write-Host " - $_"
+    }
+    $scripts | ForEach-Object {
+        LogGroup "Build local scripts - [$_]" {
+            . $_
+        }
+    }
 }
 
 $params = @{
