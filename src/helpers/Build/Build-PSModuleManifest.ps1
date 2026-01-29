aa7580e29dd56414a4ceb01657f0004b3a720d23
Marius Storhaug <marstor@hotmail.com>
2026-01-29 14:02:41 +0100

    ⚙️ [Maintenance]: Update super-linter to v8.4.0 (#128)
    This pull request primarily updates PowerShell scripts and workflow
    configuration to improve code consistency and maintainability. The main
    changes focus on standardizing string casing and operator usage,
    correcting minor typos, and updating the GitHub Actions linter version.
    
    **PowerShell Script Improvements:**
    
    * Standardized the use of `-replace` and `-split` operators to lowercase
    in several scripts.
    * Updated the conditional operator from `-NotIn` to `-notin` in
    `Build-PSModuleManifest.ps1` for proper PowerShell syntax.
    
    **Typo and Comment Corrections:**
    
    * Fixed typos in comments and attribute justifications, such as changing
    "compatability" to "compatibility" and "doesnt" to "does not" in
    `Build-PSModuleManifest.ps1` and `Build-PSModuleRootModule.ps1`.
    
    **Workflow Configuration Update:**
    
    * Updated the linter action in `.github/workflows/Linter.yml` to use
    `super-linter` version 8.4.0.
    
    **Linter Configuration:**
    
    * Added `.codespellrc` configuration file for codespell linter.

--- src/helpers/Build/Build-PSModuleManifest.ps1
+++ src/helpers/Build/Build-PSModuleManifest.ps1
@@ -101,7 +101,7 @@
 
         # Get files on module subfolders, excluding the following folders 'init', 'classes', 'public', 'private'
         $skipList = @('init', 'classes', 'functions', 'variables')
-        $ModuleOutputFolder | Get-ChildItem -Directory | Where-Object { $_.Name -NotIn $skipList } |
+        $ModuleOutputFolder | Get-ChildItem -Directory | Where-Object { $_.Name -notin $skipList } |
             Get-ChildItem -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object { $files.Add($_) }
 
         # Get the relative file path and store it in the manifest
@@ -362,7 +362,7 @@
         $tags = $PSData.Keys -contains 'Tags' ? ($PSData.Tags).Count -gt 0 ? $PSData.Tags : $repoLabels : $repoLabels
         $tags | ForEach-Object { $manifestTags.Add($_) }
         'Windows', 'Linux', 'MacOS' | ForEach-Object { $manifestTags.Add($_) }
-        # Add tags for compatability mode. https://docs.microsoft.com/en-us/powershell/scripting/developer/module/how-to-write-a-powershell-module-manifest?view=powershell-7.1#compatibility-tags
+        # Add tags for compatibility mode. https://docs.microsoft.com/en-us/powershell/scripting/developer/module/how-to-write-a-powershell-module-manifest?view=powershell-7.1#compatibility-tags
         if ($manifest.CompatiblePSEditions -contains 'Desktop') {
             if ($manifestTags -notcontains 'PSEdition_Desktop') {
                 $manifestTags.Add('PSEdition_Desktop')
